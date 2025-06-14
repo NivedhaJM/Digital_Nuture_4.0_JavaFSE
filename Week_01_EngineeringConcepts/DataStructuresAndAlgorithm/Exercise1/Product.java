c715fcf48fa2c0c923210743c7c388ff24750091
NivedhaJM <nivedhavami@gmail.com>
2025-06-14 21:57:22 +0530

    exercise-1 commit

---
+++ Week_01_EngineeringConcepts/DataStructuresAndAlgorithm/Exercise1/Product.java
@@ -0,0 +1,18 @@
+package DataStructuresAndAlgorithm.Exercise1;
+
+public class Product {
+    int productId;
+    String productName;
+    String category;
+
+    public Product(int id, String name, String category) {
+        this.productId = id;
+        this.productName = name;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return productId + " - " + productName + " [" + category + "]";
+    }
+}
---
+++ Week_01_EngineeringConcepts/DataStructuresAndAlgorithm/Exercise1/SearchAlgorithms.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SearchAlgorithms {
+
+    // Linear Search
+    public static int linearSearch(Product[] products, String targetName) {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i].productName.equalsIgnoreCase(targetName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Binary Search
+    public static int binarySearch(Product[] products, String targetName) {
+        Arrays.sort(products, Comparator.comparing(p -> p.productName.toLowerCase()));
+        int low = 0, high = products.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int cmp = products[mid].productName.compareToIgnoreCase(targetName);
+            if (cmp == 0) return mid;
+            else if (cmp < 0) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
