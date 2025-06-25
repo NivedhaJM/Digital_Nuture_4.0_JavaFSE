fd55b3cd39216f5ad11c06e91a17f09b7c0abda2
NivedhaJM <121303687+NivedhaJM@users.noreply.github.com>
2025-06-25 09:12:07 +0530

    Create CalculatorTest.java

--- Week_02 JUnit, Mockito and SL4J/JUnit_Basic Testing/Exercise-1/test/CalculatorTest.java
+++ Week_02 JUnit, Mockito and SL4J/JUnit_Basic Testing/Exercise-1/test/CalculatorTest.java
@@ -1,17 +1,19 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    @Test
-    public void testAdd() {
-        Calculator calc = new Calculator();
-        assertEquals(10, calc.add(6, 4));
-    }
+	 @Test
+	    public void testAdd() {
+	        Calculator calc = new Calculator();
+	        assertEquals(10, calc.add(6, 4));
+	    }
 
-    @Test
-    public void testSubtract() {
-        Calculator calc = new Calculator();
-        assertEquals(2, calc.subtract(6, 4));
-    }
+	    @Test
+	    public void testSubtract() {
+	        Calculator calc = new Calculator();
+	        assertEquals(2, calc.subtract(6, 4));
+	    }
 }
